fa6c7bd147193c49b99a971e742edd06d00450d9
AshutoshSundresh <ashutoshsundresh@gmail.com>
2020-11-04 20:55:12 +0530

    sdm845-common: [REVERTME] Nuke Wfd bootjar
    Signed-off-by: AshutoshSundresh <ashutoshsundresh@gmail.com>

--- common.mk
+++ common.mk
@@ -226,9 +226,6 @@ PRODUCT_PACKAGES += \
     libnl \
     WifiOverlay
 
-PRODUCT_BOOT_JARS += \
-    WfdCommon
-
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/permissions/privapp-permissions-wfd.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-wfd.xml \
     $(LOCAL_PATH)/configs/msm_irqbalance.conf:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc/msm_irqbalance.conf
