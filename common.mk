82a975f0b3ce0c3cec0122e1abe01974493feee9
zahidmgsk <zahidmgsk@gmail.com>
2020-12-27 17:04:11 +0000

    sdm845-common: update dependencies

--- common.mk
+++ common.mk
@@ -21,7 +21,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 $(call inherit-product, vendor/oneplus/sdm845-common/sdm845-common-vendor.mk)
 
 # Inherit packages from vendor/addons/oneplus/camera
-$(call inherit-product, vendor/addons/oneplus/camera/config.mk)
+#$(call inherit-product, vendor/addons/oneplus/camera/config.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
