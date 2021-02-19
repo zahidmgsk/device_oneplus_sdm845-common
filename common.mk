238f494e8e54bd5063a6a149ad28d5fb18d1c9ff
zahidmgsk <zahidmgsk@gmail.com>
2021-02-19 11:21:20 +0000

    sdm845-common: Revert:Add OnePlusGallery as Prebuilt common dt
    use vendor/addons/oneplus/apps
    
    Signed-off-by: zahidmgsk <zahidmgsk@gmail.com>

--- common.mk
+++ common.mk
@@ -21,7 +21,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 $(call inherit-product, vendor/oneplus/sdm845-common/sdm845-common-vendor.mk)
 
 # Inherit packages from vendor/addons/oneplus/apps
-#$(call inherit-product, vendor/addons/oneplus/apps/config.mk)
+$(call inherit-product, vendor/addons/oneplus/apps/config.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
@@ -165,13 +165,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += oneplus-mock
 PRODUCT_BOOT_JARS += oneplus-mock
 
-# OnePlusGallery
-PRODUCT_PACKAGES += \
-    OnePlusGallery
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/prebuilt/etc/permissions/privapp-permissions-oem.xml:system/etc/permissions/privapp-permissions-oem.xml \
-    $(LOCAL_PATH)/prebuilt/etc/sysconfig/hiddenapi-package-whitelist-oneplus.xml:system/etc/sysconfig/hiddenapi-package-whitelist-oneplus.xml
 # RCS
 PRODUCT_PACKAGES += \
     com.android.ims.rcsmanager \
