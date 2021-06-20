ab308d3f4f8ef2a29fb2e52a9d5a289a9bb5609d
Terminator-J <34873300+Terminator-J@users.noreply.github.com>
2021-06-19 21:23:40 -0700

    sdm845-common: Include prebuilt OnePlus Camera
    Signed-off-by: zahidmgsk <zahidmgsk@gmail.com>

--- common.mk
+++ common.mk
@@ -183,6 +183,15 @@ PRODUCT_PACKAGES += \
     PresencePolling \
     RcsService
 
+# OnePlus Camera
+$(call inherit-product-if-exists, packages/apps/OnePlusCamera/config.mk)
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/OnePlusCamera/hiddenapi-package-whitelist-oneplus.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/hiddenapi-package-whitelist-oneplus.xml \
+    $(LOCAL_PATH)/configs/OnePlusCamera/privapp-permissions-com.oneplus.camera.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-com.oneplus.camera.xml \
+    $(LOCAL_PATH)/configs/OnePlusCamera/privapp-permissions-com.oneplus.camera.service.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-com.oneplus.camera.service.xml \
+    $(LOCAL_PATH)/configs/OnePlusCamera/privapp-permissions-com.oneplus.gallery.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-com.oneplus.gallery.xml
+
 # Power
 PRODUCT_PACKAGES += \
     power.qcom:64
